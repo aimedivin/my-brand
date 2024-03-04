1d81a24fe782fb3bae405f2e245a907142e40580
Iadivin <aimedifi003@gmail.com>
2024-03-04 06:36:43 +0200

    feature: Finish dark mode setup for dashboard and message retrieval
    Successfully implement dark mode functionality for the dashboard and complete retrieval of sent messages from the contact form

--- js/dashboard/dashboard.js
+++ js/dashboard/dashboard.js
@@ -1,4 +1,47 @@
 const statusBlogValue = document.querySelector('.content__status_blog_value');
-console.log(statusBlogValue);
+
 const blogNumber = JSON.parse(window.localStorage.getItem('blogs'));
 statusBlogValue.innerText = blogNumber.length;
+
+// Message Display
+
+const contentDataTable = document.querySelector('.content__data_table');
+
+const dashMessage = JSON.parse(localStorage.getItem('messages'))
+
+const fargment = document.createDocumentFragment();
+
+
+for (let i = 0; i < dashMessage.length; i++) {
+    if (i == 6) break;
+
+    const tableRow = document.createElement('tr')
+
+    const col1 = document.createElement('td')
+    col1.textContent = dashMessage[i].email;
+
+    const col2 = document.createElement('td')
+    col2.textContent = dashMessage[i].subject;
+
+    const col3 = document.createElement('td')
+    const colForm = document.createElement('form')
+    const colFormBtn = document.createElement('button');
+
+    colFormBtn.classList.add('content__data_btn');
+    colFormBtn.textContent = 'Reply';
+
+    colForm.appendChild(colFormBtn)
+    col3.appendChild(colForm);
+
+    console.log('in');
+
+    tableRow.appendChild(col1);
+    tableRow.appendChild(col2);
+    tableRow.appendChild(col3);
+    fargment.appendChild(tableRow);
+}
+
+contentDataTable.appendChild(fargment);
+
+
+
